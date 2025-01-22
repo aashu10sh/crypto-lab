d7c9c80c71b52b3ffc700be7dc1eb97bc33ad4f5
Aashutosh Pudasaini <nepalidude3@gmail.com>
2025-01-22 20:51:20 +0545

    add: lab2 implementation in c++

---
+++ lab2_mono.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <unordered_map>
+
+#define MAX 300
+
+struct Message {
+  std::string message;
+};
+
+int main(void) {
+
+  std::unordered_map<char, char> map = {
+      {'a', 'm'}, {'b', 'o'}, {'c', 'q'}, {'d', 's'}, {'e', 'z'}, {'f', 'x'},
+      {'g', 'h'}, {'h', 'k'}, {'i', 'l'}, {'j', 'p'}, {'k', 'n'}, {'l', 'u'},
+      {'m', 'v'}, {'n', 'y'}, {'o', 'w'}, {'p', 'r'}, {'q', 't'}, {'r', 'a'},
+      {'s', 'j'}, {'t', 'g'}, {'u', 'd'}, {'v', 'i'}, {'w', 'e'}, {'x', 'f'},
+      {'y', 'c'}, {'z', 'b'}};
+
+  struct Message input = Message{};
+
+  std::printf("enter your message: ");
+
+  std::cin >> input.message;
+
+  std::string encrypted;
+
+  for (const char character : input.message) {
+    if (map.find(character) != map.end()) {
+      const char new_key = map[character];
+      encrypted += new_key;
+    } else {
+      std::printf(
+          "error: key not in map, please use only lowercase characters\n");
+      return -1;
+    }
+  }
+
+  printf("encrypted: %s\n", encrypted.c_str());
+
+  return 0;
+}
